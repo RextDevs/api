8a8cc70318a33a119d9e2d8c9c07893788e1adde
Kike <hernandezramirezenrique11@gmail.com>
2023-01-16 17:27:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 import json, os
 
 app = FastAPI()
-#http://127.0.0.1:8000/docs
+#http://127.0.0.1:8000/docs //redoc
 # python3 -m uvicorn main:app --reload
 
 class ModelName(str, Enum):
